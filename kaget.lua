660c03899a825ab1c02522ab7ff84bda7b5d7548
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-05-17 03:44:18 +0700

    Add initial configuration for FishIt

---
+++ kaget.lua
@@ -0,0 +1,111 @@
+_G.FishItConfig = {
+    ["Fishing"] = {
+        ["Auto Perfect"] = false,
+        ["Random Result"] = false,
+        ["Auto Favorite"] = true,
+        ["Auto Unfavorite"] = false,
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+        ["Auto Accept Trade"] = true,
+        ["Auto Friend Request"] = true,
+    },
+    ["Auto Trade"] = {
+        ["Enabled"] = true,
+        ["Whitelist Username"] = {"matnjoystok"}, 
+        ["Category Fish"] = {"Secret"},
+        ["Fish Name"] = {
+            {Name = "Ruby", Variant = "Gemstone"},
+          },
+        ["Item Name"] = {"Runic Enchant Stone"},
+    },
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false,
+        ["Target"] = 190000,
+    },
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Every"] = 27,
+    },
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Auto Element Rod"] = false,
+        ["Auto Element Rod 2"] = false,
+        ["Unlock Ancient Ruin"] = false,
+        ["Allowed Sacrifice"] = {
+            "Ghost Shark", "Cryoshade Glider", "Queen Crab", "King Crab", 
+            "Blob Shark", "Narhwal Skeleton", "King Jelly", "Mosasaur Shark", 
+            "Bone Whale", "Viridis Lurker", "Robot Kraken", "Panther Eel", 
+            "Frostborn Shark", "Thin Armor Shark", "Giant Squid", "Elshark Gran Maja"
+        },
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Treasure Room",
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["WebHook"] = {
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1448076320788447312/ghRFulPr9eMWB_noU22hs3m-uRLt77swuFHO58_JOc8UjrobTI_vEZLNL7Z0VbpEFjyX",
+        ["Auto Sending"] = true,
+        ["Category"] = {
+            "Secret", 
+            {Name = "Ruby", Variant = "Gemstone"}, 
+        },
+        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1466008452060156047/BP_G3W_ffJP51akp19IwScrj2QJdMl1TKcnFNrxe9RJkofZ36W_7GMuxlbFudosaq6vq",
+    }, 
+    ["Weather"] = {
+        ["Auto Buying"] = true,
+        ["Minimum Rod"] = "Ares Rod",
+        ["Weather List"] = {"Wind", "Snow", "Storm", "Radiant"},
+    },
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+    },
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Starter Rod",
+        ["Buy List"] = {
+            ["Mutation Totem"] = 5,
+        },
+    },
+    ["Event"] = {
+        ["Start Farm"] = true,
+        ["Minimum Rod"] = "Element Rod",
+        ["Event List"] = {
+            ["Christmas Cave"] = false,
+        },
+    },
+    ["Enchant"] = {
+        ["Auto Enchant"] = false,
+        ["Roll Enchant"] = false,
+        ["Evolved Roll Enchant"] = false,
+        ["Enchant List"] = {
+            "Reeler I", "SECRET Hunter", "Shark Hunter", "Mutation Hunter III", 
+            "Leprechaun II", "Mutation Hunter II", "Prismatic I", "Big Hunter I", "Empowered",
+        },
+        ["Second Enchant"] = false,
+        ["Allowed Sacrifice"] = {"Frostborn Shark", "Cryoshade Glider", "Blob Shark"},
+        ["Second Enchant List"] = {"Reeler I", "Prismatic I", "Mutation Hunter II"},
+        ["Minimum Rod"] = "Element Rod",
+    },
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {"Midnight Bait", "Corrupt Bait", "Aether Bait", "Singularity Bait"},
+    },
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {"Grass Rod", "Luck Rod", "Midnight Rod", "Astral Rod", "Ares Rod"},
+        ["Location Rods"] = {
+            ["Kohana Volcano"] = {"Starter Rod", "Grass Rod", "Luck Rod"},
+            ["Treasure Room"] = {"Midnight Rod", "Astral Rod"},
+            ["Underwater City"] = {"Element Rod", "Ghostfinn Rod"},
+        },
+    },
+    ["ExtremeFpsBoost"] = false,
+    ["UltimatePerformance"] = false,
+    ["Disable3DRender"] = false,
+    ["AutoRemovePlayer"] = false,
+    ["AutoReconnect"] = false,
+    ["HideGUI"] = false,
+    ["EXIT_MAP_IF_DISCONNECT"] = false,
+}
