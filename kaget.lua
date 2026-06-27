bcd0e969c3f826e8b4064bd4d3f5e2b57954aeb0
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-06-27 12:27:34 +0700

    Change Treasure Room to Ancient Ruin for rods

--- kaget.lua
+++ kaget.lua
@@ -100,7 +100,7 @@ _G.FishItConfig = {
         ["Location Rods"] = {
             ["Kohana Volcano"] = {"Starter Rod", "Grass Rod", "Luck Rod"},
             ["Treasure Room"] = {"Midnight Rod", "Astral Rod"},
-            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod"},
+            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod"},
         },
     },
     ["ExtremeFpsBoost"] = false,
