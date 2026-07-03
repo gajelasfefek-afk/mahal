27d5b12e3c5ac2fbe550e743fd1cc2c672d0c10b
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-07-03 07:03:17 +0700

    Rename 'Ancient Ruin' to 'Underwater City'

--- kaget.lua
+++ kaget.lua
@@ -100,7 +100,7 @@ _G.FishItConfig = {
         ["Location Rods"] = {
             ["Kohana Volcano"] = {"Starter Rod", "Grass Rod", "Luck Rod"},
             ["Treasure Room"] = {"Midnight Rod", "Astral Rod"},
-            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod"},
+            ["Underwater City"] = {"Element Rod", "Ghostfinn Rod"},
         },
     },
     ["ExtremeFpsBoost"] = false,
