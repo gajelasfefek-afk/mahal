8920dbcef13f050f9726d3dd042b5db7bb295d26
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-08-10 17:24:59 +0700

    Enable Auto Aetherion Bait option

--- kaget.lua
+++ kaget.lua
@@ -35,7 +35,7 @@ _G.FishItConfig = {
         ["Auto Element Rod"] = false,
         ["Auto Element Rod 2"] = true,
         ["Auto Diamond Rod"] = false,
-        ["Auto Aetherion Bait"] = false,
+        ["Auto Aetherion Bait"] = true,
         ["Unlock Ancient Ruin"] = true,
         ["Allowed Sacrifice"] = {
             "Ghost Shark", "Cryoshade Glider", "Queen Crab", "King Crab", 
