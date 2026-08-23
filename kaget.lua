b1930f694c5e92f486baa619b59f1d4752d3deef
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-08-23 12:42:26 +0700

    Change location of Elemental Island to Ancient Ruin

--- kaget.lua
+++ kaget.lua
@@ -102,7 +102,7 @@ _G.FishItConfig = {
         ["Location Rods"] = {
             ["Kohana Volcano"] = {"Starter Rod", "Grass Rod", "Luck Rod"},
             ["Treasure Room"] = {"Midnight Rod", "Astral Rod"},
-            ["Elemental Island"] = {"Element Rod", "Ghostfinn Rod"},
+            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod"},
         },
     },
     ["ExtremeFpsBoost"] = false,
